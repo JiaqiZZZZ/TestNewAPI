cfde540aa2e4dd5a5d38cc1fa7bbce7f768b1107
张嘉祺 <zhangjiaqi@MacBook-Pro.local>
2023-09-21 23:50:47 +0800

    remove all files

--- src/main/java/com/citi/techfest/ikigai/tsc/util/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.citi.techfest.ikigai.tsc.util;
-
-public class Constants {
-    public static final String NAVIGATOR = "Navigator";
-    public static final String CASEWORKER = "Case Worker";
-    public static final String BENEFICIARY = "Beneficiary";
-}
